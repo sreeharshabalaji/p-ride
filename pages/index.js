39650048edff0628af48133438cad557bee69d32
Bharath M U <bharathmogaveer01@gmail.com>
2021-12-27 19:33:03 +0530

    Validate Register Page

---
+++ pages/index.js
@@ -0,0 +1,17 @@
+import Head from "next/head";
+
+export default function Home() {
+  return (
+    <div>
+      <Head>
+        <title>P-Ride - A decentralized peer to peer ridesharing system</title>
+        <meta
+          name="description"
+          content="A decentralized peer to peer ridesharing system"
+        />
+        <link rel="icon" href="/favicon.ico" />
+        <h1></h1>
+      </Head>
+    </div>
+  );
+}
